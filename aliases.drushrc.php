ea89a63cdcce5ef266575a60076c64a39de628bd
Rafal <kenorb@gmail.com>
2014-10-01 20:07:19 +0100

    Improvements to aliases.drushrc.php example file.

--- aliases.drushrc.php
+++ aliases.drushrc.php
@@ -4,8 +4,14 @@
  * @file
  *   Alias file for drush command line tool.
  *
+ * There are several ways to create alias files:
+ *  - Put multiple aliases in a single file called aliases.drushrc.php
+ *  - Put each alias in a separate file called ALIASNAME.alias.drushrc.php
+ *  - Put groups of aliases into files called GROUPNAME.aliases.drushrc.php
+ *
  * See:
  *  http://drush.ws/docs/shellaliases.html
+ *  $ drush topic docs-aliases
  *
  *  Example usage:
  *    drush @dev status
@@ -19,22 +25,15 @@
  *
  */
 $aliases['global'] = array(
-    'ssh-options' => "-p 1022",
     // 'ssh-options' => "-p 1022 -F %root/scripts/example/conf/ssh/config" . DRUPAL_ROOT,
     'path-aliases' => array(
       '%files'   => 'sites/default/files',
-      '%private' => 'sites/default/private',
+      '%private' => 'sites/default/private/files',
     ),
 
-    'source-command-specific' => array (
-      'sql-sync' => array (
-        'structure-tables-key' => 'common',
-        'skip-tables-key' => 'common'
-      ),
-    ),
-    // target-command-specific
-    'command-specific' => array (
-      'sql-sync' => array (
+    // These options will only be set if the alias is used with the specified command.
+    'command-specific' => array(
+      'sql-sync' => array(
         'cache' => TRUE,
         'create-db' => TRUE,
         'sanitize' => TRUE,
@@ -49,20 +48,31 @@ $aliases['global'] = array(
           'common' => array('field_deleted_revision_63', 'field_deleted_revision_62', 'field_deleted_revision_60', 'field_deleted_data_60', 'field_deleted_data_63', 'field_deleted_revision_61', 'field_deleted_data_62', 'field_deleted_data_61', 'field_deleted_data_59', 'field_deleted_revision_59'),
         ),
       ),
-      'sql-dump' => array (
+      'sql-dump' => array(
         'ordered-dump' => TRUE,
         'structure-tables-key' => 'common',
         'skip-tables-key' => 'common',
-        'structure-tables' => array(
-          // You can add more tables which contain data to be ignored by the database dump
-          'common' => array('cache', 'cache_filter', 'cache_menu', 'cache_page', 'history', 'search_index', 'sessions', 'watchdog'),
-        ),
-        'skip-tables' => array(
-          'common' => array('field_deleted_revision_63', 'field_deleted_revision_62', 'field_deleted_revision_60', 'field_deleted_data_60', 'field_deleted_data_63', 'field_deleted_revision_61', 'field_deleted_data_62', 'field_deleted_data_61', 'field_deleted_data_59', 'field_deleted_revision_59'),
-        ),
       ),
-      'rsync' => array('mode' => 'rlptzO', 'verbose' => TRUE, 'no-perms' => TRUE, 'exclude' => '*.gz', 'ssh-options' => '-F config'),
+      'rsync' => array(
+          'mode' => 'rlptzO', // Single-letter rsync options are placed in the 'mode' key instead of adding '--mode=rultvz' to drush rsync command.
+          'verbose' => TRUE,
+          'no-perms' => TRUE,
+          'exclude' => "'*.gz'", // Wrapping an option's value in "" preserves inner '' on output, but is not always required.
+        # 'exclude-from' => "'/etc/rsync/exclude.rules'", // If you need multiple exludes, use an rsync exclude file.
+          'ssh-options' => '-F config',
+          'filter' => "'exclude *.sql'", // Filter options with white space must be wrapped in "" to preserve the inner ''.
+        # 'filter' => "'merge /etc/rsync/default.rules'", // If you need multple filter options, see rsync merge-file options.
+          ),
     ), // end: command-specific
+
+    // Applied only if the alias is used as the source.
+    'source-command-specific' => array(
+    ),
+
+    // Applied only if the alias is used as the target.
+    'target-command-specific' => array(
+    ),
+
 );
 
 /**
